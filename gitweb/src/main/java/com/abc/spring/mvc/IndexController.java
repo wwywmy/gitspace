4b7e39f3d4ea539df450f1891ce41f5ac2c728c3
wwywmy <378297869@qq.com>
2019-03-18 21:02:59 +0800

    add springmvc

---
+++ gitweb/src/main/java/com/abc/spring/mvc/IndexController.java
@@ -0,0 +1,14 @@
+package com.abc.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	private String index() {
+		return "springmvc";
+	}
+}
