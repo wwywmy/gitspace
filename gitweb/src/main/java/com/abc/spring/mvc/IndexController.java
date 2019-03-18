9dc48d1dcbf688b1023cbc76e631c392da45a71e
wwywmy <378297869@qq.com>
2019-03-18 21:21:29 +0800

    add logback

--- gitweb/src/main/java/com/abc/spring/mvc/IndexController.java
+++ gitweb/src/main/java/com/abc/spring/mvc/IndexController.java
@@ -1,5 +1,9 @@
 package com.abc.spring.mvc;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
+	private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	private String index() {
+		
+		logger.info("com.abc.spring.mvc.IndexController.index 当前时间:{}",new Date());
+		
 		return "springmvc";
 	}
 }
