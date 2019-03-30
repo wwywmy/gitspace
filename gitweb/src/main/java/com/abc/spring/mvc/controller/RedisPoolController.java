d469ab46dc03ecf1b3b5233c142b30b5093c0e5a
wwywmy <378297869@qq.com>
2019-03-30 09:24:27 +0800

    去除自动扫描redis目录逻辑

--- gitweb/src/main/java/com/abc/spring/mvc/controller/RedisPoolController.java
+++ gitweb/src/main/java/com/abc/spring/mvc/controller/RedisPoolController.java
@@ -1,5 +1,7 @@
 package com.abc.spring.mvc.controller;
 
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.abc.spring.redis.dao.RedisDao;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @Api(tags = "REDISPool")
 @Controller
@@ -21,7 +23,7 @@ public class RedisPoolController {
 	private static Logger logger = LoggerFactory.getLogger(RedisPoolController.class);
 
 	@Autowired
-	private RedisDao redisDao;
+	private JedisPool jedisPool;
 
 	@ApiOperation(value = "redisPool index", httpMethod = "GET")
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
@@ -32,12 +34,21 @@ public class RedisPoolController {
 		
 		StringBuffer buffer = new StringBuffer();
 		
-		redisDao.set("k1_spring_redis", "v1_spring_redis");
-		redisDao.set("k2_spring_redis", "v2_spring_redis");
-		redisDao.set("k3_spring_redis", "v3_spring_redis");
-		redisDao.set("k4_spring_redis", "v4_spring_redis");
+		Jedis jedis = jedisPool.getResource();
+		
+		jedis.set("k1_spring_redis", "v1_spring_redis");
+		jedis.set("k2_spring_redis", "v2_spring_redis");
+		jedis.set("k3_spring_redis", "v3_spring_redis");
+		jedis.set("k4_spring_redis", "v4_spring_redis");
 		
 		
+		Set<String> set = jedis.keys("*");
+		for(String s: set) {
+			buffer.append(s+"="+jedis.get(s));
+		}
+		
+		
+		//jedis.disconnect();
 		
 		
 		
